58fa491c7672bb7d723963cfbd9a7d3ed68753b8
vinivsb <vinicius.vsb@gmail.com>
2014-11-25 10:54:43 -0200

    change url root

--- rateconverter/urls.py
+++ rateconverter/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'rateconverter.views.home', name='home'),
+    url(r'^$', 'portal.views.index', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^home/', include('portal.urls')),
     url(r'^admin/', include(admin.site.urls)),
